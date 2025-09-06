c35ff67afefabb27e6036d94d1387ac4404a7e98
tmansuroglu <mansuroglu99@gmail.com>
2025-09-07 01:37:50 +0300

    refactor: seperated FormCheckboxInput component into 2 parts

---
+++ app/components/checkbox-input/CheckboxInput.tsx
@@ -0,0 +1,73 @@
+import {
+  ChangeEventHandler,
+  LabelHTMLAttributes,
+  ReactNode,
+  useState,
+} from "react";
+import { RefCallBack } from "react-hook-form";
+
+export type CheckboxInputProps = {
+  LabelProps?: LabelHTMLAttributes<HTMLLabelElement> & {
+    ref: RefCallBack;
+  };
+  inputSuffix?: ReactNode;
+  caption?: ReactNode;
+  name?: string;
+  disabled?: boolean;
+  label: ReactNode;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  defaultIsChecked?: boolean;
+};
+
+export function CheckboxInput({
+  inputSuffix,
+  caption,
+  name,
+  disabled,
+  label,
+  onChange,
+  LabelProps,
+  defaultIsChecked = false,
+}: CheckboxInputProps) {
+  const [isChecked, setIsChecked] = useState(defaultIsChecked);
+
+  return (
+    <div>
+      <label
+        {...LabelProps}
+        className={`
+    relative block px-4 py-3 rounded-[27px] border no-underline text-left min-w-32 font-sm outline-0
+    ${disabled ? "bg-brand-light-gray cursor-not-allowed" : "cursor-pointer"}
+    ${
+      isChecked
+        ? "bg-brand-secondary-black text-brand-primary-white hover:bg-brand-secondary-gray hover:text-brand-primary-white"
+        : "text-brand-secondary-black hover:bg-brand-light-gray hover:text-brand-secondary-black"
+    }
+    ${LabelProps?.className}
+  `}
+        tabIndex={0}
+        htmlFor={name}
+        aria-disabled={disabled}
+      >
+        <span>{label}</span>
+        {/* // TODO: cant select it with keyboard */}
+        <input
+          type="checkbox"
+          name={name}
+          id={name}
+          className="hidden"
+          onChange={(e) => {
+            setIsChecked(e.target.checked);
+            onChange(e);
+          }}
+        />
+        {inputSuffix && (
+          <span className=" absolute right-3 bottom-0.5 -translate-y-full pointer-events-none">
+            {inputSuffix}
+          </span>
+        )}
+      </label>
+      {caption && caption}
+    </div>
+  );
+}
--- app/components/form-checkbox-input/FormCheckboxInput.tsx
+++ app/components/form-checkbox-input/FormCheckboxInput.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { ChangeEventHandler, ReactNode } from "react";
 import { useController, useFormContext } from "react-hook-form";
+import CheckboxInput from "../checkbox-input";
 
 type FormCheckboxInputProps = {
   hideErrorText?: boolean;
@@ -48,64 +49,48 @@ export function FormCheckboxInput({
   };
 
   return (
-    <div>
-      <label
-        htmlFor={name}
-        aria-disabled={disabled}
-        className={`
-  relative block
-  px-4 py-3 rounded-[27px] border no-underline text-left min-w-32 font-sm outline-0
-  ${disabled ? "bg-brand-light-gray cursor-not-allowed" : "cursor-pointer"}
-  ${
-    isChecked
-      ? "bg-brand-secondary-black text-brand-primary-white hover:bg-brand-secondary-gray hover:text-brand-primary-white"
-      : "text-brand-secondary-black hover:bg-brand-light-gray hover:text-brand-secondary-black"
-  }
-  ${fieldErrorMessage ? "border-error" : "border-brand-secondary-black"}
-`.trim()}
-        ref={ref}
-        tabIndex={0}
-      >
-        <span>{label}</span>
-        {/* // TODO: cant select it with keyboard */}
-        <input
-          type="checkbox"
-          name={name}
-          id={name}
-          className="hidden"
-          onChange={handleChange}
-        />
-        <span className=" absolute right-3 bottom-0.5 -translate-y-full pointer-events-none">
-          {!!fieldErrorMessage ? (
-            <Image
-              src="/error.svg"
-              width={16}
-              height={16}
-              alt="error icon"
-              className="w-4 h-4"
-            />
-          ) : isChecked ? (
-            <Image
-              src="/white-tick.svg"
-              width={16}
-              height={16}
-              className="w-4 h-4"
-              alt="checked icon"
-            />
-          ) : (
-            <Image
-              src="/plus.svg"
-              width={16}
-              height={16}
-              alt="plus icon"
-              className="w-4 h-4"
-            />
-          )}
-        </span>
-      </label>
-      {Boolean(!!fieldErrorMessage && !hideErrorText) && (
-        <span className="error-text">{fieldErrorMessage}</span>
-      )}
-    </div>
+    <CheckboxInput
+      label={label}
+      name={name}
+      onChange={handleChange}
+      LabelProps={{
+        ref,
+        className: fieldErrorMessage
+          ? "border-error"
+          : "border-brand-secondary-black",
+      }}
+      inputSuffix={
+        !!fieldErrorMessage ? (
+          <Image
+            src="/error.svg"
+            width={16}
+            height={16}
+            alt="error icon"
+            className="w-4 h-4"
+          />
+        ) : isChecked ? (
+          <Image
+            src="/white-tick.svg"
+            width={16}
+            height={16}
+            className="w-4 h-4"
+            alt="checked icon"
+          />
+        ) : (
+          <Image
+            src="/plus.svg"
+            width={16}
+            height={16}
+            alt="plus icon"
+            className="w-4 h-4"
+          />
+        )
+      }
+      caption={
+        Boolean(!!fieldErrorMessage && !hideErrorText) && (
+          <span className="error-text">{fieldErrorMessage}</span>
+        )
+      }
+    />
   );
 }
