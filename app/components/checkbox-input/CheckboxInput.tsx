1390e2e36c2cd6870f62e07951895b4d282ae176
tmansuroglu <mansuroglu99@gmail.com>
2025-09-08 01:02:43 +0300

    feat: improved CheckboxInput accessibility

--- app/components/checkbox-input/CheckboxInput.tsx
+++ app/components/checkbox-input/CheckboxInput.tsx
@@ -1,5 +1,6 @@
 import {
   ChangeEventHandler,
+  KeyboardEventHandler,
   LabelHTMLAttributes,
   ReactNode,
   useState,
@@ -33,12 +34,25 @@ export function CheckboxInput({
 }: CheckboxInputProps) {
   const [isChecked, setIsChecked] = useState(defaultIsChecked);
 
+  const handleOnKeyDown: KeyboardEventHandler<HTMLLabelElement> = (e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      e.preventDefault();
+      const input = e.currentTarget.querySelector(
+        'input[type="checkbox"]'
+      ) as HTMLInputElement;
+      if (input && !disabled) {
+        input.click();
+      }
+    }
+  };
+
   return (
     <div>
       <label
         {...LabelProps}
         className={`
-    relative block px-4 py-3 rounded-[27px] border no-underline text-left min-w-32 font-sm outline-0
+    relative block px-4 py-3 rounded-[27px] border text-left min-w-32 font-sm outline-0
+    focus:outline
     ${disabled ? "bg-brand-light-gray cursor-not-allowed" : "cursor-pointer"}
     ${
       isChecked
@@ -47,9 +61,11 @@ export function CheckboxInput({
     }
     ${LabelProps?.className}
   `}
+        aria-live="polite"
         tabIndex={0}
         htmlFor={name}
         aria-disabled={disabled}
+        onKeyDown={handleOnKeyDown}
       >
         <span>{label}</span>
         {/* // TODO: cant select it with keyboard */}
@@ -58,6 +74,7 @@ export function CheckboxInput({
           name={name}
           id={name}
           aria-describedby={ariaDescribedBy}
+          aria-checked={isChecked}
           className="hidden"
           onChange={(e) => {
             setIsChecked(e.target.checked);
