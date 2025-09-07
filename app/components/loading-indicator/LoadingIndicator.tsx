1cef342ce57b2251a6642c00860cedb1e2793277
tmansuroglu <mansuroglu99@gmail.com>
2025-09-08 01:40:30 +0300

    feat: improved LoadingIndicator  accessibility

--- app/components/loading-indicator/LoadingIndicator.tsx
+++ app/components/loading-indicator/LoadingIndicator.tsx
@@ -1,6 +1,18 @@
-export function LoadingIndicator() {
+type LoadingIndicatorProps = {
+  ariaLabel?: string;
+};
+
+export function LoadingIndicator({
+  ariaLabel = "Loading...",
+}: LoadingIndicatorProps) {
   return (
-    <div className="flex justify-center items-center">
+    <div
+      className="flex justify-center items-center"
+      role="status"
+      aria-label={ariaLabel}
+      aria-live="polite"
+      aria-hidden
+    >
       <div className="w-4 h-4 border-2 border-t-blue-500 border-gray-200 rounded-full animate-spin" />
     </div>
   );
