3151e59d2c0b8dd9f382b20bc420e584b4b562e9
tmansuroglu <mansuroglu99@gmail.com>
2025-09-11 00:41:25 +0300

    feat: created error boundary

---
+++ app/error.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { redirect, RedirectType } from "next/navigation";
+import { Routes } from "./types/enums";
+import PageContainer from "./components/page-container";
+
+export default function Error({}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <PageContainer className="flex flex-col gap-10 container mx-auto justify-center items-center">
+      <h1 className="font-xl xl:font-2xl text-center">Something went wrong!</h1>
+      <button
+        className="btn-primary w-2xs"
+        onClick={() => redirect(Routes.Business, RedirectType.replace)}
+      >
+        Go back to main page
+      </button>
+    </PageContainer>
+  );
+}
